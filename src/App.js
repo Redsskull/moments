7d33f8b2fe2100aee05b68490e2561bdd6fbda78
Redsskull <ivry91@gmail.com>
2024-02-11 15:52:35 +0100

    created functioning navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import styles from './App.module.css';
+import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import {Route,Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Hello World</h1>
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Routes>
+          <Route path = "/" element={<h1>Home Page</h1>} />
+          <Route path = "/signin" element={<h1>Sign In</h1>}/>
+          <Route path = "/signup" element={<h1>Sign up</h1>}/>
+          <Route path="*" element={<p>Page not found!</p>} />
+        </Routes>
+      </Container>
     </div>
   );
 }
