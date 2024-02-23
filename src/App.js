6a94b1da947256ea8950343d7373fe16691d3e82
Redsskull <ivry91@gmail.com>
2024-02-23 20:27:00 +0100

    add many posts componenets

--- src/App.js
+++ src/App.js
@@ -6,17 +6,30 @@ import './api/axiosDefaults';
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
 import PostCreateForm from './pages/posts/PostCreateForm';
+import PostPage from './pages/posts/PostPage';
+import PostsPage from './pages/posts/PostsPage';
+import { useCurrentUser } from './contexts/CurrentUserContext';
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile?.id || "";
+
+
   return (
         <div className={styles.App}>
           <NavBar />
           <Container className={styles.Main}>
             <Routes>
-              <Route path = "/" element={<h1>Home Page</h1>} />
+            <Route path = "/" element={<PostsPage message = "No results found. Adjust the search keyword. "/>} />
+              <Route path = "/feed" element={<PostsPage message = "No results found. Adjust the search keyword or follow a user "
+              filter={`owner__followed__owner__profile=${profile_id}&`}/>} />
+               <Route path = "/liked" element={<PostsPage message = "No results found. Adjust the search keyword or like a post "
+              filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}/>} />
+
               <Route path = "/signin" element={<SignInForm/>}/>
               <Route path = "/signup" element={<SignUpForm/>}/>
               <Route path = "/posts/create" element={<PostCreateForm/>}/>
+              <Route path = "/posts/:id" element={<PostPage/>}/>
               <Route path="*" element={<p>Page not found!</p>} />
             </Routes>
           </Container>
---
+++ src/pages/posts/PostsPage.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import NoResults from "../../assets/no_results.svg";
+import appStyles from "../../App.module.css";
+import styles from "../../styles/PostsPage.module.css";
+import { useLocation } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+import Asset from "../../components/Asset";
+import Post from "./Post";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
+
+function PostsPage({message, filter = ""}) {
+    const [posts, setPosts] = useState({results: []});
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const {pathname} = useLocation();
+
+    const [query, setQuery] = useState(""); 
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const {data} = await axiosReq.get(`/posts/?${filter} search=${query}`);
+                setPosts(data);
+                setHasLoaded(true);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+   setHasLoaded(false);
+   const timer = setTimeout(() => {
+         fetchPosts();
+    }, 1000);
+    return () => clearTimeout(timer);
+    }, [filter, query, pathname]);
+  
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p>Popular profiles mobile</p>
+        < i className = {`fas fa-search $ {styles.SearchIcon}`} /> 
+        <Form className={styles.SearchBar}
+        onSubmit = {(e) => e.preventDefault()} >
+            <Form.Control 
+            value = {query}
+            onChange = {(e) => setQuery(e.target.value)}
+            type = "text" 
+            className="mr-sm-2" 
+            placeholder = "Search Posts" />
+        </Form>
+        {hasLoaded ? (
+          <>
+            {posts.results.length ? (
+                <InfiniteScroll
+                children = {
+                    posts.results.map((post) => (
+                        <Post key={post.id} {...post} setPosts={setPosts} />
+                      ))
+                }
+                dataLength = {posts.results.length}
+                loader = {<Asset spinner />}
+                hasMore = {!!posts.next}
+                next = {()=> fetchMoreData(posts, setPosts)}
+                />
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
+      </Col>
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+        <p>Popular profiles for desktop</p>
+      </Col>
+    </Row>
+  );
+}
+
+export default PostsPage;
